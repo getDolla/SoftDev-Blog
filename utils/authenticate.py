717e5215244e5a228ef56c6a34cf5b0293f111b1
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-31 17:32:00 -0400

    I think authenticate.py is finished. I am going to work on app.py to add some of the authenticate stuff now

--- utils/authenticate.py
+++ utils/authenticate.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 from os import path, remove
 
 def accessDB( ):
@@ -10,16 +11,41 @@ def accessDB( ):
     db.close()
     return d
 
-def authenticate( requestForm, databaseData ):
+def authenticate( requestForm, userNames, passWords ):    
+    if( requestForm['user'] in userNames ):
+        index = userNames.index(requestForm['user'])
+        if passWords[index] == hashWord( requestForm['password'] ):
+            return True
+        else:
+            return "Bad Password"
+    else:
+        return "Bad Username"
+
+def register( requestForm, userNames, passWords ):
+    if( requestForm['user'] in userNames ):
+        return False
+    else:
+        #userNames.append( requestForm['user'] )
+        #passWords.append( hashWord(requestForm['password']) )
+        addToDB( requestForm['user'], hashWord(requestForm['password']))
+        return True
+
+def hashWord( strIn ):
+    return hashlib.sha256(strIn).hexdigest()
+    
+def dbHandler( databaseData ):
     userNames = []
     passWords = []
     for row in databaseData:
         userNames.append( row[0] );
         passWords.append( row[1] );
-    if( requestForm['user'] in userNames ):
-        index = userNames.index(requestForm['user'])
-        if passWords[index] == hashWord( requestForm['password'] ):
-            #do something
-        else:
+    return { 'usernames' : userNames, 'passwords' : passWords }
 
+def commitToDB( userName, passWord ):
+    db = sqlite3.connect("../data/database.db")
+    cursor = db.cursor()
+    cmd = "INSERT INTO users VALUES ( '%s', '%s', '')"%(userName, passWord)
+    cursor.execute(cmd)
+    db.commit()
+    db.close()
 

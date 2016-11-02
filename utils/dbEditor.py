78333af68cc222dae1f2021436aaf3655dfe77ec
esandine <esandine@stuy.edu>
2016-11-01 20:27:21 -0400

    Adding to stories works

--- utils/dbEditor.py
+++ utils/dbEditor.py
@@ -35,7 +35,26 @@ def createStory(user, title, submission):
     db.close()
     return True;
 
-
+#addStory(user, title, submission)
+#updates the record in stories, and updates the user's stories edited
+#Params:
+#    user - username who added to the story
+#    story_id - the story that is edited
+#    text - the text added to the story
+#Returns True
+def addStory(user, story_id, text):
+    db = sqlite3.connect("../data/database.db")
+    c = db.cursor()
+    #Gets new story
+    q = "SELECT story FROM stories WHERE id=%d"%(story_id)
+    stories = c.execute(q)
+    for record in stories:
+        story = record[0] # Woohoo dynamic typing
+    story+=text
+    q = "UPDATE stories SET time = %f, last_submission = '%s', story = '%s' WHERE id = %d"%(time.time(), text, story, story_id)
+    c.execute(q)
+    db.commit()
+    db.close()
 if __name__=='__main__':
-    print createStory("ely2", "titletry2", "story2")
+    addStory("ely", 1, " newtext")
     

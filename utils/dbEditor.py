4b059411a4e6efe460ad485d352bf7b285b17576
esandine <esandine@stuy.edu>
2016-11-01 20:58:54 -0400

    Adding and creating now edits the users list

--- utils/dbEditor.py
+++ utils/dbEditor.py
@@ -11,50 +11,75 @@ def insertID(story_ids, newstory):
     l.append(str(newstory))
     retstr=""
     for story in l:
-        retstr+=story
-        retstr+=","
+        if(len(story)>0):
+            retstr+=story
+            retstr+=","
     retstr=retstr[0:-1]
     return retstr
+
+#touchStory(user, story_id)
+#adds a story id to the list of stories the user has edited
+#Params:
+#    string user - the user who edited a story
+#    int story_id - the story id to add
+#Returns True
+def touchStory(user, story_id):
+    db = sqlite3.connect("../data/database.db")
+    c = db.cursor()
+    #Gets old story id list and adds to it
+    q = "SELECT story_ids FROM users WHERE username='%s'"%(user)
+    storyids = c.execute(q)
+    for record in storyids:
+        newstoryid = record[0] # Woohoo dynamic typing
+    newstoryid=insertID(newstoryid, story_id)
+    #updates record
+    q = "UPDATE users SET story_ids = '%s' WHERE username = '%s'"%(newstoryid,user)
+    c.execute(q)
+    db.commit()
+    db.close()
 
 #createStory(user, title, submission)
 #creates a new story record in stories
 #Params:
-#    user - username who created story
-#    title - title of story
-#    submission - the text submitted
+#    string user - username who created story
+#    string title - title of story
+#    string submission - the text submitted
 #    note the story id is generated using the current time and random
 #Returns True
 def createStory(user, title, submission):
     db = sqlite3.connect("../data/database.db")
     c = db.cursor()
     random.seed(time.time())
-    q = "SELECT COUNT(*) FROM stories"
-    q = "INSERT INTO stories VALUES (%d, '%s', %f, '%s', '%s')"%(random.randint(0,int(time.time())), title,time.time(),submission, submission)
+    story_id=random.randint(0,int(time.time()))
+    q = "INSERT INTO stories VALUES (%d, '%s', %f, '%s', '%s')"%(story_id, title,time.time(),submission, submission)
     c.execute(q)
     db.commit()
     db.close()
+    touchStory(user, story_id)
     return True;
 
 #addStory(user, title, submission)
 #updates the record in stories, and updates the user's stories edited
 #Params:
-#    user - username who added to the story
-#    story_id - the story that is edited
-#    text - the text added to the story
+#    string user - username who added to the story
+#    int story_id - the story that is edited
+#    string text - the text added to the story
 #Returns True
 def addStory(user, story_id, text):
     db = sqlite3.connect("../data/database.db")
     c = db.cursor()
-    #Gets new story
+    #Gets old story and adds new story
     q = "SELECT story FROM stories WHERE id=%d"%(story_id)
     stories = c.execute(q)
     for record in stories:
-        story = record[0] # Woohoo dynamic typing
+        story = record[0]
     story+=text
+    #updates record
     q = "UPDATE stories SET time = %f, last_submission = '%s', story = '%s' WHERE id = %d"%(time.time(), text, story, story_id)
     c.execute(q)
     db.commit()
     db.close()
+    touchStory(user, story_id)
+
 if __name__=='__main__':
-    addStory("ely", 1, " newtext")
-    
+    addStory("dota", 0, "This is a test of stories")
